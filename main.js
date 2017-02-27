96a44483ab4a234905893c80ae3d64b47818c84a
Uros <uros.bacinic@gmail.com>
2017-02-27 20:04:49 +0100

    new stuff

---
+++ main.js
@@ -0,0 +1,13 @@
+var urke = new Object({
+  name: 'Uros',
+  age: 31,
+  bio: function(){
+    console.log('Hi my name is fckn ' + urke.name)
+  }
+});
+
+urke.bio()
+
+var person2 = Object.create(urke);
+
+person2.bio()
